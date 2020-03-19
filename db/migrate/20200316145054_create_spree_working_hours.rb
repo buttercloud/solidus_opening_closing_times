e26a1968df0a57c4d354af37f533c191d3f732bd
Islam Odeh <islamodeh@hotmail.com>
2020-03-19 11:56:21 +0200

    Use datetime instead of time for working hours.

--- db/migrate/20200316145054_create_spree_working_hours.rb
+++ db/migrate/20200316145054_create_spree_working_hours.rb
@@ -1,8 +1,8 @@
 class CreateSpreeWorkingHours < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_working_hours do |t|
-      t.time :start_time
-      t.time :end_time
+      t.datetime :start_time
+      t.datetime :end_time
       t.integer :working_day_id
       t.timestamps
     end
